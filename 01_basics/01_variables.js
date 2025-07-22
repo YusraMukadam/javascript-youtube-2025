09dc1108ee6915f6bc7be9a7ac0fcddae0ee478a
YusraMukadam <yusramukadam123@gmail.com>
2025-07-22 12:08:56 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1222234
+let userEmail= "yusramukadam@gmail.com"
+var userPassword="yusra28298"
+accountHandler="female"
+let accountState;
+//accountId=2
+//console.log(accountId)
+userEmail="yusra@gmail.com"
+userPassword=122234555
+accountHandler="male"
+console.table([userEmail,userPassword,accountHandler,accountState])
+/* 
+prefer not to use var becuz of issue in block scope and functional scope
+*/
+
