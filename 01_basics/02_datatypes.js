00e4c5490e6fee03d886f856d8ee690cd331e551
YusraMukadam <yusramukadam123@gmail.com>
2025-07-22 12:06:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+//many datatypes r there bigint for big numbers 
+//null-- standalone value
+//string--""
+//boolean true or false
+//undefined-- the value is not assigned yet
+let account;
+console.log(typeof account);
+console.log(typeof null);
