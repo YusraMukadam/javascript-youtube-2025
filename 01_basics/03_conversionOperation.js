0e4f7602ad4c696b259c9b001177a2b1126557cb
YusraMukadam <yusramukadam123@gmail.com>
2025-07-25 16:28:51 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+/*let info="33absd";
+console.log(typeof info);
+console.log(typeof(info));
+let MustBeNum=Number(info)
+console.log(typeof(MustBeNum));
+console.log(MustBeNum);*/
+let BoolVal=0;
+console.log(Boolean(BoolVal));
+//"yusra"-->true
+// ""-->false
+//1=true 0=false
+let SomeNumber=33
+let StringVal=String(SomeNumber)
+console.log(StringVal);
+console.log(typeof StringVal);
